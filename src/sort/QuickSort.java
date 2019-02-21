ca247dcac9b4be0dc67ba096af71dd104f8af207
lukaszwelnicki <lukasz.welnicki@gmail.com>
2019-02-21 21:51:05 +0100

    Implemented quicksort

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,19 @@
+package sort;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import static sort.SortUtils.partition;
+
+public class QuickSort {
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+}
--- src/sort/SortUtils.java
+++ src/sort/SortUtils.java
@@ -23,4 +23,28 @@ public class SortUtils {
             else a[k] = aux[i++];
         }
     }
+
+    static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo]))
+                if (i == hi) break;
+
+            while (less(a[lo], a[--j]))
+                if (j == lo) break;
+
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+
+        exch(a, lo, j);
+        return j;
+    }
+
+    static void exch(Comparable[] a, int from, int to) {
+        Comparable toObj = a[to];
+        a[to] = a[from];
+        a[from] = toObj;
+    }
+
 }
